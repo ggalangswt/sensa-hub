f324c671600c09c100ae9392ff1e3b3bd9c05216
ggalangswt <swastikaramadhan05@gmail.com>
2026-06-13 12:12:56 +0700

    feat: implement core game logic, matchmaking, and player services for the Sensa platform

---
+++ app/play/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Play from '@/src/modules/play/Play'
+import AppLayout from "@/src/components/layouts/AppLayout"
+
+const page = () => {
+  return (
+    <AppLayout>
+      <Play />
+    </AppLayout>
+  )
+}
+
+export default page
